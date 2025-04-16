31c03adcebc2f34b0f5b6cdb4499ba89dc4b032a
Neil Swart <neil.swart@brutex.com>
2025-04-16 09:07:19 +0200

    cleanup icons

--- src/core/theme/index.ts
+++ src/core/theme/index.ts
@@ -1,7 +1,9 @@
 import { createSystem, defaultConfig } from '@chakra-ui/react';
-import * as mdIcons from 'react-icons/md';
+// import * as mdIcons from 'react-icons/md';
 
 export const themeSystem = createSystem(defaultConfig);
-export const themeIcons = mdIcons;
+// export const themeIcons = mdIcons;
+export const themeIcons = {};
 
-export type ThemeIcon = keyof typeof themeIcons;
+// export type ThemeIcon = keyof typeof themeIcons;
+export type ThemeIcon = string;
