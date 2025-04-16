9dfa5406a66bc2deab19ed8597d538acb21f59e4
Neil Swart <neil.swart@brutex.com>
2025-04-16 08:40:17 +0200

    switch favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +9,10 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.ts",
-      registerType: "autoUpdate",
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
       injectRegister: false,
 
       pwaAssets: {
@@ -21,73 +21,75 @@ export default defineConfig({
       },
 
       manifest: {
-        name: "Budgety",
-        short_name: "budgety",
-        description: "Budget like a boss",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#0ec4e6",
-        theme_color: "#173da6",
+        name: 'Budgety',
+        short_name: 'budgety',
+        description: 'Budget like a boss',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0ec4e6',
+        theme_color: '#173da6',
         related_applications: [
           {
-            platform: "webapp",
-            url: "https://budgety-pwa.vercel.app/manifest.json",
+            platform: 'webapp',
+            url: 'https://budgety-pwa.vercel.app/manifest.json',
           },
         ],
         icons: [
           {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: "/pwa-maskable-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
+            src: '/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: "/pwa-maskable-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
 
         screenshots: [
           {
-            src: "screenshot.png",
-            sizes: "382x757",
-            type: "image/png",
-            form_factor: "wide",
-            label: "Budgety",
+            src: 'screenshot.png',
+            sizes: '382x757',
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'Budgety',
           },
           {
-            src: "screenshot.png",
-            sizes: "382x757",
-            type: "image/png",
-            form_factor: "narrow",
-            label: "Budgety",
+            src: 'screenshot.png',
+            sizes: '382x757',
+            type: 'image/png',
+            form_factor: 'narrow',
+            label: 'Budgety',
           },
         ],
       },
 
       injectManifest: {
-        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
       },
+
+      includeAssets: ['favicon.png'],
 
       devOptions: {
         enabled: false,
-        navigateFallback: "index.html",
+        navigateFallback: 'index.html',
         suppressWarnings: true,
-        type: "module",
+        type: 'module',
       },
     }),
   ],
