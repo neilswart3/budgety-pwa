ed11a500564124bd0767f12cb333d23a234ae4cb
Neil Swart <neil.swart@brutex.com>
2025-09-26 08:30:31 +0200

    fix type error category card

--- src/components/ui/CategoryCard/Template.tsx
+++ src/components/ui/CategoryCard/Template.tsx
@@ -11,7 +11,7 @@ import { ReactElement } from 'react';
 import { MdChevronRight } from 'react-icons/md';
 import { Link } from 'react-router';
 
-interface Props extends Pick<ICategory, 'description'> {
+interface Props extends Partial<Pick<ICategory, 'description'>> {
   icon: ReactElement;
   label: ReactElement | string;
   tags: ReactElement;
@@ -30,48 +30,42 @@ export const CategoryCardTemplate: React.FC<Props> = ({
   avatarBg,
   link,
   description,
-}) => {
-  return (
-    <Card.Root>
-      <Card.Header
-        {...(link ? { as: Link, to: link } : {})}
-        p={6}
-        bg="bg.muted"
-      >
-        <HStack gap={6} alignItems="center">
-          <Avatar.Root
-            size="xl"
-            {...(avatarBg ? { bg: avatarBg } : {})}
-            {...(ring
-              ? {
-                  colorPalette,
-                  css: defineStyle({
-                    outlineWidth: 3,
-                    outlineColor: `${colorPalette}.500`,
-                    outlineOffset: 2,
-                    outlineStyle: 'solid',
-                  }),
-                }
-              : {})}
-          >
-            <Icon size="2xl">{icon}</Icon>
-          </Avatar.Root>
-          <Stack flex={1}>
-            <Card.Title>{label}</Card.Title>
-          </Stack>
-          {!!link && (
-            <HStack>
-              <MdChevronRight size={28} />
-            </HStack>
-          )}
-        </HStack>
-      </Card.Header>
-      <Card.Body>
-        <Stack gap={4}>
-          <Stack>{description}</Stack>
-          <HStack flexWrap="wrap">{tags}</HStack>
+}) => (
+  <Card.Root>
+    <Card.Header {...(link ? { as: Link, to: link } : {})} p={6} bg="bg.muted">
+      <HStack gap={6} alignItems="center">
+        <Avatar.Root
+          size="xl"
+          {...(avatarBg ? { bg: avatarBg } : {})}
+          {...(ring
+            ? {
+                colorPalette,
+                css: defineStyle({
+                  outlineWidth: 3,
+                  outlineColor: `${colorPalette}.500`,
+                  outlineOffset: 2,
+                  outlineStyle: 'solid',
+                }),
+              }
+            : {})}
+        >
+          <Icon size="2xl">{icon}</Icon>
+        </Avatar.Root>
+        <Stack flex={1}>
+          <Card.Title>{label}</Card.Title>
         </Stack>
-      </Card.Body>
-    </Card.Root>
-  );
-};
+        {!!link && (
+          <HStack>
+            <MdChevronRight size={28} />
+          </HStack>
+        )}
+      </HStack>
+    </Card.Header>
+    <Card.Body>
+      <Stack gap={4}>
+        <Stack>{description}</Stack>
+        <HStack flexWrap="wrap">{tags}</HStack>
+      </Stack>
+    </Card.Body>
+  </Card.Root>
+);
